37a54d1e2ea17d1af066fcec17f770823c387947
Lucas Neiva <112989505+lucasneiva@users.noreply.github.com>
2024-06-09 10:52:25 -0300

    funfando blz

--- ProjetoFinal/js/modal.js
+++ ProjetoFinal/js/modal.js
@@ -1,4 +1,6 @@
-// modal.js
+import { createTask, updateTask, generateUniqueId } from "./task.js";
+import { renderTasks } from '../script.js';
+
 const taskModal = document.getElementById( 'taskModal' );
 const closeModalBtn = document.querySelector( '.close-modal' );
 const taskForm = document.getElementById( 'taskForm' );
@@ -9,38 +11,33 @@ const taskAreaSelect = document.getElementById( 'taskArea' );
 const taskModalTitle = document.getElementById( 'taskModalTitle' );
 const taskPrioritySelect = document.getElementById( 'taskPriority' );
 
-let currentModalMode = 'create'; // 'create' or 'edit' or 'view'
+let currentModalMode = 'create';
 
-// Function to open the modal
-function openTaskModal( task = {}, mode = 'create' ) {
+function openTaskModal( tasks, task = {}, mode = 'create' ) {
     currentModalMode = mode;
-    // Populate modal fields
 
-    if (task) {
+    if ( task ) {
         taskIdInput.value = task.id || '';
         taskTitleInput.value = task.title || '';
         taskDueDateInput.value = task.dueDate || '';
-        taskAreaSelect.value = task.area || 'Work';
-        taskPrioritySelect.value = task.priority || 'medium';        
+        taskAreaSelect.value = task.area || 'Profissional';
+        taskPrioritySelect.value = task.priority || 'medium';
     }
 
     if ( currentModalMode === 'view' ) {
-        // Make form fields read-only
         taskTitleInput.readOnly = true;
         taskDueDateInput.readOnly = true;
         taskAreaSelect.disabled = true;
         taskPrioritySelect.disabled = true;
         taskModalTitle.innerHTML = "Ver Tarefa";
-        // Hide the "Save Task" button
         taskForm.querySelector( 'button[type="submit"]' ).style.display = 'none';
-    } else if ( currentModalMode === 'edit') {
+    } else if ( currentModalMode === 'edit' ) {
         taskTitleInput.readOnly = false;
         taskDueDateInput.readOnly = false;
         taskAreaSelect.disabled = false;
         taskPrioritySelect.disabled = false;
         taskForm.querySelector( 'button[type="submit"]' ).style.display = 'block';
         taskModalTitle.innerHTML = "Editar Tarefa";
-
     } else {
         taskTitleInput.readOnly = false;
         taskDueDateInput.readOnly = false;
@@ -48,23 +45,43 @@ function openTaskModal( task = {}, mode = 'create' ) {
         taskPrioritySelect.disabled = false;
         taskForm.querySelector( 'button[type="submit"]' ).style.display = 'block';
         taskModalTitle.innerHTML = "Criar Tarefa";
-
     }
+
+    taskForm.onsubmit = function ( event ) {
+        event.preventDefault();
+
+        const newTask = {
+            id: taskIdInput.value || generateUniqueId(),
+            title: taskTitleInput.value,
+            dueDate: taskDueDateInput.value,
+            area: taskAreaSelect.value,
+            priority: taskPrioritySelect.value,
+            status: 'active'
+        };
+
+        if ( currentModalMode === 'create' ) {
+            tasks = createTask( tasks, newTask.title, newTask.dueDate, newTask.area, newTask.status, newTask.priority );
+        } else if ( currentModalMode === 'edit' ) {
+            tasks = updateTask( tasks, newTask.id, newTask.title, newTask.dueDate, newTask.area, newTask.status, newTask.priority );
+        }
+
+        renderTasks( tasks );
+        closeTaskModal();
+    };
+
     taskModal.style.display = 'block';
 }
 
 function closeTaskModal() {
-    taskForm.reset();
-    currentModalMode = 'create';
-
-    taskTitleInput.readOnly = false;
-    taskDueDateInput.readOnly = false;
-    taskAreaSelect.disabled = false;
-    taskPrioritySelect.disabled = false;
-    taskForm.querySelector( 'button[type="submit"]' ).style.display = 'block';
-
     taskModal.style.display = 'none';
 }
 
 closeModalBtn.addEventListener( 'click', closeTaskModal );
+
+window.addEventListener( 'click', ( event ) => {
+    if ( event.target === taskModal ) {
+        closeTaskModal();
+    }
+} );
+
 export { openTaskModal, closeTaskModal };
--- ProjetoFinal/script.js
+++ ProjetoFinal/script.js
@@ -2,51 +2,39 @@ import { createTask, updateTask, deleteTask, loadTasks } from './js/task.js';
 import { openTaskModal } from './js/modal.js';
 
 const taskAreaContainer = document.getElementById( 'taskAreaContainer' );
-const createTaskBtn = document.getElementById('createTaskBtn');
+const createTaskBtn = document.getElementById( 'createTaskBtn' );
 
 let tasks = [];
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener( 'DOMContentLoaded', () => {
     tasks = loadTasks();
-    renderTasks(tasks);
-});
+    renderTasks( tasks );
+} );
 
-function renderTasks( tasks ) {
-
+export function renderTasks( tasks ) {
     const taskAreas = {
         Profissional: [],
         Pessoal: [],
         Acadêmica: []
     };
 
-    // cria e salva taskcards para cada area
-
     tasks.forEach( task => {
         const taskCardHTML = `
-      <div class="task-card" data-task-id="${task.id}">
-        <h3>${task.title}</h3>
-        <p>Término: ${task.dueDate}</p>
-        <span class="status-indicator ${task.status === 'active' ? 'active' : 'inactive'}"></span> 
-        <span class="priority-indicator ${task.priority}"></span> 
-        <button class="open-task-modal-btn">Abrir</button>
-        <button class="edit-task-btn">Editar</button>
-        <button class="delete-task-btn">Deletar</button>
-      </div>
-    `;
-
+            <div class="task-card" data-task-id="${task.id}">
+                <h3>${task.title}</h3>
+                <p>Término: ${task.dueDate}</p>
+                <span class="status-indicator ${task.status === 'active' ? 'active' : 'inactive'}"></span> 
+                <span class="priority-indicator ${task.priority}"></span> 
+                <button class="open-task-modal-btn">Abrir</button>
+                <button class="edit-task-btn">Editar</button>
+                <button class="delete-task-btn">Deletar</button>
+            </div>
+        `;
         taskAreas[ task.area ].push( taskCardHTML );
-
-        
     } );
-
-
-
-
-    // renderiza todas as areas com seus respectivos taskcards
 
     for ( const area in taskAreas ) {
         const areaDiv = document.querySelector( `.task-area[data-area="${area}"]` );
-
         areaDiv.innerHTML = `<h2>${area}</h2>`;
 
         if ( taskAreas[ area ].length > 0 ) {
@@ -56,10 +44,7 @@ function renderTasks( tasks ) {
         } else {
             areaDiv.innerHTML += '<p>Sem tarefas por enquanto.</p>';
         }
-
     }
-
-    // Adicionar event listenners
 
     const openTaskButtons = document.querySelectorAll( '.open-task-modal-btn' );
     const editTaskButtons = document.querySelectorAll( '.edit-task-btn' );
@@ -70,7 +55,7 @@ function renderTasks( tasks ) {
             const taskCard = event.target.closest( '.task-card' );
             const taskId = taskCard.dataset.taskId;
             const taskToView = tasks.find( task => task.id === taskId );
-            openTaskModal( taskToView, 'view' );
+            openTaskModal( tasks, taskToView, 'view' );
         } );
     } );
 
@@ -79,7 +64,7 @@ function renderTasks( tasks ) {
             const taskCard = event.target.closest( '.task-card' );
             const taskId = taskCard.dataset.taskId;
             const taskToUpdate = tasks.find( task => task.id === taskId );
-            openTaskModal( taskToUpdate, 'edit' );
+            openTaskModal( tasks, taskToUpdate, 'edit' );
         } );
     } );
 
@@ -87,26 +72,14 @@ function renderTasks( tasks ) {
         button.addEventListener( 'click', ( event ) => {
             const taskCard = event.target.closest( '.task-card' );
             const taskId = taskCard.dataset.taskId;
-            // Ask for confirmation before deleting
             if ( confirm( 'Are you sure you want to delete this task?' ) ) {
                 tasks = deleteTask( tasks, taskId );
-                renderTasks( tasks ); // Re-render tasks after deletion
+                renderTasks( tasks );
             }
         } );
     } );
 }
 
-createTaskBtn.addEventListener('click', () => {
-    // When the create task button is clicked, 
-    // open the modal in 'create' mode (this is assumed to be the default 
-    // in your openTaskModal function, but you can specify it explicitly if needed).
-    openTaskModal({}, 'create', (taskId, title, dueDate, area, priority) => {
-        // After the modal is submitted, create the task:
-        tasks = createTask(tasks, title, dueDate, area, 'active', priority); // Assuming 'active' as a default status
-        // And re-render the tasks:
-        renderTasks(tasks);
-    });
-}); 
-
-
-renderTasks(tasks);
+createTaskBtn.addEventListener( 'click', () => {
+    openTaskModal( tasks, {}, 'create' );
+} );
